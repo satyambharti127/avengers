32734c2433ffc317f59047d34ba8125eec16b622
alex <alejandro.gonzalez@galileo.edu>
2025-09-05 00:24:52 -0700

    mongo orm updated

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,13 @@ import mongoose from 'mongoose'
 const app = express()
 const port = 8000
 
+
+
 app.get('/', (req, res) => {
   res.send('test!')
 })
+
+await mongoose.connect(process.env.MONGOURL);
 
 app.listen(port, () => {
 
