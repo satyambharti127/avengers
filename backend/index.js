1266a7be238a396e696f931ca523dae1daec16ff
agarwalsujal <sujalagarwa26@gmai.com>
2025-09-05 13:08:38 +0530

    Fix environment variable naming and establish MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 import "dotenv/config"
 import express from 'express'
 import mongoose from 'mongoose'
+import connectDB from "./config/db.config.js"
 const app = express()
 const port = process.env.PORT || 8000
 
 
-
+connectDB();
 app.get('/', (req, res) => {
   res.send('test!')
 })
 
-await mongoose.connect(process.env.MONGOURL);
+
 
 app.listen(port, () => {
 
